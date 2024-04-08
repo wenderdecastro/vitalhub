e8056af86222f3d94a002e9eb50fae27f3cfa948
Wender <wendermaiac@gmail.com>
2024-04-08 09:19:22 -0300

    save

--- webAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ webAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d94d015d2e4abf47534fa8b1d9fcb13c046d5f2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+960d1bcd8cede249eeb521b6d7292ce1943df11d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
