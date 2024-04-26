802c2970b48bff7830b0d79574b563ac8df46d18
Wender <wendermaiac@gmail.com>
2024-04-26 08:52:32 -0300

    save

--- vitalHub/src/components/BoxInput/index.js
+++ vitalHub/src/components/BoxInput/index.js
@@ -1,62 +1,62 @@
-import {
-	FieldContent,
-	InputLabel,
-	InputText,
-	InputTextModificate,
-} from './style';
+import { FieldContent, InputLabel, InputText, InputTextModificate } from "./style"
 
 export const BoxInput = ({
-	fieldWidth = 100,
-	fieldHeight = 50,
-	editable = false,
-	textLabel,
-	placeholder,
-	fieldValue,
-	onChangeText = null,
-	keyType = 'default',
-	maxLenght,
-	onBlur = null,
-	multiline,
-	keyboardType,
-	insertRecord,
+    fieldWidht = 100,
+    fieldHeight = 50,
+    editable = false,
+    textLabel,
+    placeholder,
+    fieldValue,
+    onChangeText = null,
+    keyType = 'default',
+    maxLenght,
+    onBlur = null,
+    multiline,
+    keyboardType,
+    insertRecord
 }) => {
-	return (
-		<FieldContent fieldWidth={fieldWidth}>
-			<InputLabel>{textLabel}</InputLabel>
+    return (
+        <FieldContent
+            fieldWidht={fieldWidht}
+        >
+            <InputLabel>{textLabel}</InputLabel>
 
-			{insertRecord ? (
-				<>
-					<InputTextModificate
-						fieldWidth={fieldWidth}
-						editable={editable}
-						placeholder={placeholder}
-						fieldValue={fieldValue}
-						onChangeText={onChangeText}
-						keyType={keyType}
-						maxLenght={maxLenght}
-						onBlur={onBlur}
-						fieldHeight={fieldHeight}
-						multiline={multiline}
-						keyboardType={keyboardType}
-					/>
-				</>
-			) : (
-				<>
-					<InputText
-						fieldWidth={fieldWidth}
-						editable={editable}
-						placeholder={placeholder}
-						fieldValue={fieldValue}
-						onChangeText={onChangeText}
-						keyType={keyType}
-						maxLenght={maxLenght}
-						onBlur={onBlur}
-						fieldHeight={fieldHeight}
-						multiline={multiline}
-						keyboardType={keyboardType}
-					/>
-				</>
-			)}
-		</FieldContent>
-	);
-};
+            {insertRecord ? (
+            <>
+            <InputTextModificate
+                editable={editable}
+                placeholder={placeholder}
+                fieldValue={fieldValue}
+                onChangeText={onChangeText}
+                keyType={keyType}
+                maxLenght={maxLenght}
+                onBlur={onBlur}
+                fieldHeight={fieldHeight}
+                multiline={multiline}
+                keyboardType={keyboardType}
+            />
+            </>
+            ) : (
+            <>
+            <InputText
+                editable={editable}
+                placeholder={placeholder}
+                fieldValue={fieldValue}
+                onChangeText={onChangeText}
+                keyType={keyType}
+                maxLenght={maxLenght}
+                onBlur={onBlur}
+                fieldHeight={fieldHeight}
+                multiline={multiline}
+                keyboardType={keyboardType}
+                
+            />
+            </>
+            )}
+
+            
+        </FieldContent>
+    )
+
+
+}
